bba282264a9e77dd553d1b14bc6a6c22fd741e55
Adrian Mota <adrianmota152@gmail.com>
2022-12-16 09:57:07 -0400

    Fixes some logic in the is-citize-auth.js file

--- middleware/is-citizen-auth.js
+++ middleware/is-citizen-auth.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
   if (!req.session.isCitizenLoggedIn) {
-    res.render("home/index", {
+    return res.render("home/index", {
       title: "Home",
       hasError: true,
       errorMessage: "No tienes permitido entrar en este apartado",
