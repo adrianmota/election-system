185c9b77f5e78c3053a13a6ac30b55024e8977f8
Adrian Mota <adrianmota152@gmail.com>
2022-12-16 11:13:01 -0400

    Fixes some issues

--- controllers/voteController.js
+++ controllers/voteController.js
@@ -145,6 +145,7 @@ exports.postCreate = (req, res, next) => {
           .catch((err) => console.error(err));
       })
       .catch((err) => console.error(err));
+      return;
   }
 
   Candidate.findOne({
@@ -157,7 +158,6 @@ exports.postCreate = (req, res, next) => {
       Election.findOne({ where: { status: true } })
         .then((result) => {
           const election = result.dataValues;
-          candidateId = candidateId ? candidateId : null;
 
           Vote.create({
             CitizenId: req.citizen.dataValues.id,
